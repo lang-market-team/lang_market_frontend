eab8210ff6858eecb9efcdb6891accdf56276d09
thanhln <lenhatthanh100@gmail.com>
2020-12-14 18:22:48 +0700

    Update login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import "./pages.css";
 import LoginBox from '../components/LoginBox';
+import logoLangMarket from '../images/Logo_LangMarket.PNG'
 class Login extends Component {
   render() {
-    return <LoginBox />;
+    return (
+      <div>
+        <div className='row'>
+          <div className='col-md-7'>
+            <img src={logoLangMarket} alt='Logo'/>
+          </div>
+          <div className="col-md-5 login-box-area">
+            <LoginBox />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 export default Login;
