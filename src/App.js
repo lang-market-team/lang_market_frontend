cf09e19eb0a5d3b0ebd5c2b81334af4341cd8ce9
thanhln <lenhatthanh100@gmail.com>
2020-12-20 16:56:17 +0700

    Update navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import NavbarComponent from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <Router>
       <div>
-        <Navbar />
+        <NavbarComponent />
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
