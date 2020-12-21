31b8c2bdda0ad8f87a41414c650ca339b91bc795
dungcodejob <dungcodejob@gmail.com>
2020-12-21 16:31:32 +0700

    add cart

---
+++ src/components/common/cart-product.js
@@ -0,0 +1,68 @@
+import Star from "./star";
+import "../../css/cart-product.css";
+const CartProduct = () => {
+    return (
+        <div className="cart-product">
+            <div className="cart-img">
+
+            </div>
+            <div className="cart-info">
+                <div className="cart-left">
+                    <div className="cart-product-name">
+                        Tên sản phẩm
+            </div>
+                    <div className="cart-product-description">
+                        mô tả sơ lược về sản phẩm bày bán
+            </div>
+                    <div className="cart-product-rating">
+                        <Star width="18" height="18" isActive="true" />
+                        <Star width="18" height="18" isActive="true" />
+                        <Star width="18" height="18" isActive="true" />
+                        <Star width="18" height="18" isActive="true" />
+                        <Star width="18" height="18" isActive="false" />
+                    </div>
+
+                    <div className="cart-product-supplier">
+                        <span>Cung cấp bởi</span> Zara
+            </div>
+                    <div className="cart-product-status">
+                        <span>Tình trạng</span> mới
+            </div>
+                    <div className="cart-product-trademark">
+                        <span>Thương hiệu</span> OEM
+            </div>
+                </div>
+                <div className="cart-right">
+                    <div className="cart-right-top">
+                        <div className="cart-product-price">
+                            <div className="product-new-price">
+                                1.400.000 Đ
+                    </div>
+                            <div className="product-old-price">
+                                2.000.000
+                    </div>
+                        </div>
+
+                        <div className="product-number">
+                            <div className="product-button-minus">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                            </div>
+                            <input />
+                            <div className="product-button-add">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" ><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="cart-right-bottom">
+                        Tổng tiền <span>2.800.000 Đ</span>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default CartProduct;
--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,6 +1,33 @@
+import "../css/cart.css";
+import CartProduct from "../components/common/cart-product";
 const Cart = () => {
     return (
-        <div></div>
+        <div className="cart my-container">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-9 col-12">
+                        <CartProduct />
+                        <CartProduct />
+                        <CartProduct />
+                    </div>
+                    <div className="col-lg-3 col-12">
+                        <div className="pay">
+                            <div className="pay-box">
+                                Thành tiền:
+                                <div className="pay-money">
+                                    <span>6,870,000đ</span>
+                                    (Đã bao gồm VAT nếu có)
+                                </div>
+                            </div>
+
+                            <div className="pay-button">
+                                Tiến hành đặt hàng
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
