31a159970761cbf86c5305ba03925072769da727
ducquynh0106 <48541505+ducquynh0106@users.noreply.github.com>
2020-12-25 21:43:53 +0700

    update UI

--- src/pages/order/order.js
+++ src/pages/order/order.js
@@ -3,48 +3,43 @@ import {NavLink} from "reactstrap";
 const order = () => {
     return (         
         <div className="order">
-            <div className="product-img">
-
-            </div>
             <div className="product-info">
                 <div className="product-left">
                     <div className="product-name">
-                        Tên sản phẩm
-                    </div>
-                    <div className="product-supplier">
-                        <span>Cung cấp </span> Zara
+                        <span>Mã đơn hàng: </span> ID
                     </div>
                     <div className="product-quanlity">
-                        <span>So luong: </span> 3
+                        <span>Thời gian tạo đơn hàng: </span> Time
                     </div>
-                    <div className="product-monney">
-                        <span>Tong tien: </span> 1.000.000
+                    <div className="product-quanlity">
+                        <span>Thời gian giao dự kiến: </span> Time
+                    </div>
+                    <div className="product-quanlity">
+                        <span>Tổng giá: </span> Price
+                    </div>
+                    <div className="product-quanlity">
+                        <span>Thanh toán: </span> Trước
                     </div>
                 </div>
                 <div className="product-right">
-                    <NavLink href="/order/manageorder" className="order-manager">MANAGE</NavLink>
-                    <div className="product-right-top">
-                        <div className="wrapper">
+                    <NavLink href="/order/manageorder" className="order-manager">MANAGE</NavLink>              
+                    <div className="wrapper">
                             <div className="margin-area">
                                 <div className="dot one">1</div>
                                 <div className="dot two">2</div>
                                 <div className="dot three">3</div>
                                 <div className="dot four">4</div>
-                                <div className="dot five">5</div>
                                 <div className="progress-bar first"></div>
                                 <div className="progress-bar second"></div>
                                 <div className="progress-bar third"></div>
-                                <div className="progress-bar fourx"></div>
                                 <div className="row message">
                                     <div className="message-1">Chờ xác nhận </div>
-                                    <div className="message-2">Chờ lấy hàng</div>
-                                    <div className="message-3">Đang giao</div>
-                                    <div className="message-4">Đã giao</div>
-                                    <div className="message-5">Đã hủy</div>
+                                    <div className="message-2">Đang giao</div>
+                                    <div className="message-3">Đã giao</div>
+                                    <div className="message-4">Đã hủy</div>
                                 </div>        
                             </div>
-                        </div>  
-                    </div>
+                        </div>   
                 </div>
             </div>   
         </div>
