547eef489582031b7a710e8cea07bf646a1ab9a4
ducquynh0106 <48541505+ducquynh0106@users.noreply.github.com>
2020-12-26 19:59:12 +0700

    udate order

--- src/pages/order/order.js
+++ src/pages/order/order.js
@@ -1,15 +1,36 @@
 import "../../css/order.css";
 const order = (props) => {
+    const format = "yyyy-MM-dd";
+    function convertTimeToString(time, format){
+        const date = new Date(time);
+        const year = date.getFullYear();
+        const mon = ("0" + (date.getMonth() + 1)).slice(-2);
+        const day = ("0" + date.getDate()).slice(-2);
+        const hour = ("0" + date.getHours()).slice(-2);
+        const min = ("0" + date.getMinutes()).slice(-2);
+        const second = ("0" + date.getSeconds()).slice(-2);
     
+        return format
+            .replace("YYYY", year)
+            .replace("yyyy", year)
+            .replace("dd", day)
+            .replace("DD", day)
+            .replace("MM", mon)
+            .replace("hh", hour)
+            .replace("mm", min)
+            .replace("ss", second);
+    }
+
     return (        
         <div className="order">
+
             <div className="order-product-info">
                 <div className="product-left">
                     <div className="order-product-name">
                         <span>Mã đơn hàng: </span> {props.order.id_order}
                     </div>
                     <div className="order-product-quanlity">
-                        <span>Thời gian tạo đơn hàng: </span> {props.order.create_time}
+                        <span>Thời gian tạo đơn hàng: </span> {convertTimeToString(props.order.create_time,format)}
                     </div>
                     <div className="order-product-quanlity">
                         <span>Thời gian giao dự kiến: </span> {props.order.delivery_time}
@@ -18,7 +39,7 @@ const order = (props) => {
                         <span>Tổng giá: </span> {props.order.total_price}
                     </div>
                     <div className="order-product-quanlity">
-                        <span>Thanh toán: </span> Trước
+                        <span>Thanh toán: </span> {props.order.paid_order===true?'Trước':'Sau'}
                     </div>
                 </div>
                 <div className="order-product-right">
@@ -27,8 +48,8 @@ const order = (props) => {
                     </a>         
                     <div className="wrapper">
                             <div className="margin-area">
-                                <div className="dot one">1</div>
-                                {props.order.status_order>=2 ?  <div className="dot two-active">2</div>: <div className="dot two-inactive">2</div>}
+                                <div className="dot one" >1</div>
+                                {props.order.status_order>=2 ?  <div className="dot two-active" >2</div>: <div  className="dot two-inactive">2</div>}
                                 {props.order.status_order>=3 ?  <div className="dot three-active">3</div>: <div className="dot three-inactive">3</div>}
                                 {props.order.status_order===4 ?  <div className="dot four-active">4</div>: <div className="dot four-inactive">4</div>}
                                 <div className="progress-bar first"></div>
