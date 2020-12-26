e2de0dd47404743a22bcc250efe430eb0f29e77f
ducquynh0106 <48541505+ducquynh0106@users.noreply.github.com>
2020-12-26 20:16:31 +0700

    update order

--- src/pages/order/order.js
+++ src/pages/order/order.js
@@ -20,10 +20,23 @@ const order = (props) => {
             .replace("mm", min)
             .replace("ss", second);
     }
-
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length,c.length);
+            }
+        }
+        return "";
+    }
     return (        
         <div className="order">
-
+            {console.log(getCookie('type_account'))}
             <div className="order-product-info">
                 <div className="product-left">
                     <div className="order-product-name">
