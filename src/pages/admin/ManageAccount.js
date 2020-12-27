33d118f9f560fb46d161d1af918c74eede5f3f78
vtquan0213@gmail.com <vtquan0213@gmail.com>
2020-12-27 21:42:29 +0700

    done shop and create product

--- src/pages/admin/ManageAccount.js
+++ src/pages/admin/ManageAccount.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import serverAddress from "../../serverConnection";
 
 class ManageAccount extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data : [],
+//     };
+//   }
+
+//   componentDidMount(){
+//     fetch(serverAddress + "api/user/get_user_by_type/id_buyer=", {
+//         method: "get",
+//         headers: { "Content-Type": "application/json" },
+//       })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         this.setState({data_order:data})
+//       });
+//     else if (type_account==="2"){
+//         fetch(serverAddress + "api/order/id_seller="+ca.toString(), {
+//             method: "get",
+//             headers: { "Content-Type": "application/json" },
+//         })
+//         .then((res) => res.json())
+//         .then((data) => {
+//             this.setState({data_order:data})
+//         });
+//     }       
+// }
+
   render() {
     return (
       <div>
-        Quan Ly Tai Khoan
+          áddsa
       </div>
     );
   }
--- src/pages/seller/ManageProduct.js
+++ src/pages/seller/ManageProduct.js
@@ -1,10 +1,119 @@
 import React, { Component } from 'react';
 
+import { ButtonToggle, Button, Form, FormGroup, Label, Input,Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import Cookies from 'universal-cookie';
+import serverAddress from "../../serverConnection";
+
 class ManageProduct extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      shop_details : [],
+      modal:false,
+      redirect: false,
+    };
+  }
+
+  
+
+  openModal = () => {
+    this.setState({modal: !this.state.modal});
+  }
+
+  redirectCreate = () => {
+    window.location.href = "/seller/create_product"
+  }
+
+
+  // changeMethod = () => {
+  //   const cookies = new Cookies();
+  //   const {shop_name, shop_describe}=this.state;
+  //   console.log(shop_name);
+  //   console.log(shop_describe);
+  //   console.log(cookies.get('id_user').toString())
+  //   fetch(serverAddress + "api/shop/get/id_user="+ cookies.get('id_user').toString(), {
+  //     method: "post",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       id_user: cookies.get('id_user').toString(),
+  //       shop_name: shop_name,
+  //       shop_describe: shop_describe,
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  // }
+
+  componentDidMount(){
+    const cookies = new Cookies();
+    fetch(serverAddress + "api/shop/get/id_user="+ cookies.get('id_user').toString(), {
+        method: "get",
+        headers: { "Content-Type": "application/json" },
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        this.setState({shop_details:data})
+        console.log(this.state.shop_details)
+    }); 
+  }
+
   render() {
+    const {shop_details}=this.state
     return (
       <div>
-        Quan Ly San Pham
+        <div className="my-container">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="order-title">
+                            Danh sách sản phẩm
+                        </div>
+                    </div>
+                    <div className="col-lg-9 col-12">
+                      <p>Tên cửa hàng: <strong>{shop_details.shop_name}</strong></p>
+                      <p>Mô tả: {shop_details.shop_describe}</p>
+                    </div>
+                </div>
+                <div className="">
+                <ButtonToggle onClick={this.redirectCreate} color="primary">Thêm sản phẩm</ButtonToggle>{' '}
+                <ButtonToggle onClick={this.openModal} color="primary">Chỉnh sửa sản phẩm</ButtonToggle>{' '}
+                <Modal isOpen={this.state.modal}>
+                    <ModalHeader >Chỉnh sửa</ModalHeader>
+                    <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Label for="shop_name">Tên mới</Label>
+                            <Input
+                            type="text"
+                            name="shop_name"
+                            id="shop_name"
+                            placeholder="Nhập tên gian hàng"
+                            onChange={this.changeName}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="shop_describe">Mô tả mới</Label>
+                            <Input
+                            type="text"
+                            name="shop_describe"
+                            id="shop_describe"
+                            placeholder="Nhập mô tả"
+                            onChange={this.changeDescription}
+                            />
+                        </FormGroup>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                    <Button color="primary" onClick={this.changeMethod}>Lưu</Button>{' '}
+                    <Button color="secondary" onClick={this.openModal}>Hủy</Button>
+                    </ModalFooter>
+                </Modal>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
